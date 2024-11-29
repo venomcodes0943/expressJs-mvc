c68319413b64d5768d0ea04b2772df932346f034
Haseeb <dhcodes0943@gmail.com>
2024-11-29 23:01:08 +0500

    minor updates

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 import express, { json, urlencoded } from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import mongoose from "mongoose";
 
 var app = express();
 
+// Cors configurations
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Routers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  console.log({ err });
+  res.json(err);
+});
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+  console.log("Successfully connected to the database");
+});
 
 export default app;
